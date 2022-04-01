34120708e06686bbea4d7c35f2f3de70bda6115c
AdrianValladares <a.valladares.d@gmail.com>
2022-04-01 10:07:57 +0200

    wrote getTotalStarships function

---
+++ js/starWarsApiUtils/getTotalShips.js
@@ -0,0 +1,7 @@
+const getTotalShips = async () => {
+  const response = await fetch("https://swapi.dev/api/starships");
+  const data = await response.json();
+
+  return data.count;
+};
+export default getTotalShips;

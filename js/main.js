67fa04ed1a54094029220997140e2a201203c890
AdrianValladares <a.valladares.d@gmail.com>
2022-04-01 10:14:52 +0200

    included starship count

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 import getTotalShips from "./starWarsApiUtils/getTotalShips.js";
 
 const starshipCount = document.querySelector("#total-ships");
-console.log(starshipCount);
 const nOfStarships = await getTotalShips();
 starshipCount.textContent = `Total ships: ${nOfStarships}`;
